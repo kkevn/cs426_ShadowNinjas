f8ddb262ffdb043b67b63b5af384c204183bb061
Kevin Kowalski <43073295+kkowal28@users.noreply.github.com>
2020-04-05 23:29:12 -0500

    added base game

---
+++ ShadowNinjas/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float xOffset = 0f;
+    public float yOffset = 10f;
+    public float zOffset = 10f;
+
+    public Transform Player;
+
+    // Update is called once per frame
+    void Update() {
+
+        // follow player's position with given offsets
+        transform.position = new Vector3(Player.position.x + xOffset, Player.position.y + yOffset, Player.position.z + zOffset);
+    }
+}
