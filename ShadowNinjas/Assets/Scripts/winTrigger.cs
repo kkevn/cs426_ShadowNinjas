1a3262ce1c15b96e7040cee748362480b31b3f75
Kevin Kowalski <43073295+kkowal28@users.noreply.github.com>
2020-04-08 01:55:19 -0500

    added patrol AI and more
    - fixed enemy torch walk animation
    - fix win trigger for player collision only

--- ShadowNinjas/Assets/Scripts/winTrigger.cs
+++ ShadowNinjas/Assets/Scripts/winTrigger.cs
@@ -1,18 +1,18 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class winTrigger : MonoBehaviour
-{
+public class winTrigger : MonoBehaviour {
     public GameObject levelCompleteMsg;
-    private void OnTriggerEnter(Collider other)
-    {
-        levelCompleteMsg.SetActive(true);
-        StartCoroutine(Close(5));
+
+    // Trigger a win if the player enters the region
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
+            levelCompleteMsg.SetActive(true);
+            StartCoroutine(Close(5));
+        }
     }
 
-    public IEnumerator Close(float x)
-    {
+    public IEnumerator Close(float x) {
         yield return new WaitForSeconds(x);
         Application.Quit();
     }
