7760d48e3575205ff2387c2f3f10a29bef662805
Kevin Kowalski <43073295+kkowal28@users.noreply.github.com>
2020-04-08 16:04:26 -0500

    skybox, archer anim, level_0 castle

--- ShadowNinjas/Assets/Scripts/EnemyShoot.cs
+++ ShadowNinjas/Assets/Scripts/EnemyShoot.cs
@@ -1,34 +1,43 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyShoot : MonoBehaviour
-{
+public class EnemyShoot : MonoBehaviour {
     public GameObject arrow;
     public Transform arrowSpawner;
     public float shootForce = 20f;
+    public float maxTimeBetweenShots = 2.0f;
     private float elapsed = 0f;
     private Vector3 force = new Vector3(0, 1, 10);
     private AudioSource audioS;
 
+    private Animator anim;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         audioS = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         elapsed += Time.deltaTime;
+        
+        // shoot every 5 (length of animation) to maxTimeBeweenShots seconds
+        if (elapsed >= Random.Range(5.0f, 5.0f + maxTimeBetweenShots)) {
+            anim.SetTrigger("Shoot");
 
-        if(elapsed >= Random.Range(1.5f, 6f))
-        {
-            GameObject shoot = Instantiate(arrow, arrowSpawner.position, Quaternion.identity);
-            Rigidbody rb = shoot.GetComponent<Rigidbody>();
-            rb.velocity = force * shootForce;
-            audioS.Play();
+            // arrow release in animation happens at 3.2seconds
+            StartCoroutine(Shoot(3.2f));
             elapsed = 0f;
         }
+    }
+
+    // spawn and shoot the arrow after waiting x seconds
+    public IEnumerator Shoot(float x) {
+        yield return new WaitForSeconds(x);
+        GameObject shoot = Instantiate(arrow, arrowSpawner.position, Quaternion.identity);
+        Rigidbody rb = shoot.GetComponent<Rigidbody>();
+        rb.velocity = force * shootForce;
+        audioS.Play();
     }
 }
