ee85a37d436bdad21b57f2c35ad33265c57db217
mperko6 <55112448+mperko6@users.noreply.github.com>
2020-04-07 21:54:17 -0500

    Added enemies firing flame arrows into targets with sound, crate texture, walking animation

---
+++ ShadowNinjas/Assets/Scripts/EnemyShoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    public GameObject arrow;
+    public Transform arrowSpawner;
+    public float shootForce = 20f;
+    private float elapsed = 0f;
+    private Vector3 force = new Vector3(0, 1, 10);
+    private AudioSource audioS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioS = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsed += Time.deltaTime;
+
+        if(elapsed >= Random.Range(1.5f, 6f))
+        {
+            GameObject shoot = Instantiate(arrow, arrowSpawner.position, Quaternion.identity);
+            Rigidbody rb = shoot.GetComponent<Rigidbody>();
+            rb.velocity = force * shootForce;
+            audioS.Play();
+            elapsed = 0f;
+        }
+    }
+}
