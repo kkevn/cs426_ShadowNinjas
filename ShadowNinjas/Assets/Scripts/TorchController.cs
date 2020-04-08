d33baa61d327bc728c51d2c42f419f08ae209d8e
Kevin Kowalski <43073295+kkowal28@users.noreply.github.com>
2020-04-07 20:26:21 -0500

    torch improvements

--- ShadowNinjas/Assets/Scripts/TorchController.cs
+++ ShadowNinjas/Assets/Scripts/TorchController.cs
@@ -16,6 +16,9 @@ public class TorchController : MonoBehaviour {
 
     private Light[] lights;
 
+    private bool wasHit = false;
+    private bool isExtinguished = false;
+
     // Start is called before the first frame update
     void Start() {
 
@@ -25,16 +28,36 @@ public class TorchController : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        
+
+        // if flagged as hit
+        if (/*lights[0].enabled &&*/ wasHit) {
+
+            foreach (Light light in lights) {
+
+                // decrease angle and intensity of light
+                light.spotAngle -= 0.05f;
+                light.range -= 0.5f;
+                light.intensity -= 0.05f;
+
+                // disable lights
+                if (light.spotAngle <= 0) {
+                    light.enabled = false;
+                    wasHit = false;
+                    isExtinguished = true;
+                }
+            }
+        }
     }
 
-    // When hit by object tagged "Weapon", remove light source from the torch
+    // When lit and hit by object tagged "Weapon", destroy light source
     private void OnCollisionEnter(Collision other) {
-        if (other.gameObject.CompareTag("Weapon")) {
-            foreach (Light light in lights) {
-                light.enabled = false;
-                system.Stop(true, ParticleSystemStopBehavior.StopEmitting);
-            }
+        if (other.gameObject.CompareTag("Weapon") && isExtinguished == false) {
+
+            // flag as hit
+            wasHit = true;
+
+            // stop particle system
+            system.Stop(true, ParticleSystemStopBehavior.StopEmitting);
         }
     }
 }
