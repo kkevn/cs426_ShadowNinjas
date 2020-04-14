49c04f35bdc23ba1144ff721e570690a9bcd0b1f
Kevin Kowalski <43073295+kkowal28@users.noreply.github.com>
2020-04-14 03:14:29 -0500

    added main menu

---
+++ ShadowNinjas/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void StartGame() {
+        SceneManager.LoadScene("Level_0");
+    }
+
+    public void ShowControls() {
+        Debug.Log("Not yet implemented.");
+        //todo
+    }
+
+    public void ExitGame() {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
