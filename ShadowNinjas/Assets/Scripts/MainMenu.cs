0d1732b558f4ba2b106d689fda30c3be0c0b8fa8
Kevin Kowalski <43073295+kkowal28@users.noreply.github.com>
2020-04-22 01:10:14 -0500

    improved controls menu and fixed dirt material on flooring

--- ShadowNinjas/Assets/Scripts/MainMenu.cs
+++ ShadowNinjas/Assets/Scripts/MainMenu.cs
@@ -5,6 +5,9 @@ public class MainMenu : MonoBehaviour {
 
     public GameObject main;
     public GameObject controls;
+    public GameObject controls1;
+    public GameObject controls2;
+    public GameObject controls3;
     public GameObject cutscene;
 
 
@@ -16,13 +19,31 @@ public class MainMenu : MonoBehaviour {
     }
 
     public void ShowControls() {
-        main.SetActive(false);
+        controls3.SetActive(false);
         controls.SetActive(true);
+    }
+
+    public void ShowMovement() {
+        main.SetActive(false);
+        controls1.SetActive(true);
+    }
+
+    public void ShowAiming() {
+        controls1.SetActive(false);
+        controls2.SetActive(true);
+    }
+
+    public void ShowLives() {
+        controls2.SetActive(false);
+        controls3.SetActive(true);
     }
 
     public void LeaveControls() {
         main.SetActive(true);
         controls.SetActive(false);
+        controls1.SetActive(false);
+        controls2.SetActive(false);
+        controls3.SetActive(false);
     }
 
     public void ExitGame() {
