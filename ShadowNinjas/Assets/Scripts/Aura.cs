2c0cbec8faa38af9b880afb907e8ac2b7c5db7e7
Kevin Kowalski <43073295+kkowal28@users.noreply.github.com>
2020-04-28 21:41:25 -0500

    improved ammo with aura and sound

---
+++ ShadowNinjas/Assets/Scripts/Aura.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Aura : MonoBehaviour {
+
+    public AudioClip clip;
+
+    private void OnTriggerEnter(Collider other) {
+
+        // play sound at position when player picks up ammo
+        if (other.gameObject.CompareTag("Player")) {
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+        }
+    }
+}
